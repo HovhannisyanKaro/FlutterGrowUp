a85e486a478aec8e4d3dc3f445b89c120422bf4e
Karo Hovhannisyan <hovhannisyankaro1992@gmail.com>
2023-11-16 15:24:01 +0400

    Animated BottomNavigationBar

--- lib/learning/widgets/bottomnavigationbar.dart
+++ lib/learning/widgets/bottomnavigationbar.dart
@@ -89,7 +89,7 @@ const _buildBody = <Widget>[HomeMenu(), ProductsMenu(), ProfileMenu()];
 //   }
 // }
 
-class HomeMenu extends StatelessWidget{
+class HomeMenu extends StatelessWidget {
   const HomeMenu({super.key});
 
   @override
@@ -99,7 +99,7 @@ class HomeMenu extends StatelessWidget{
   }
 }
 
-class ProductsMenu extends StatelessWidget{
+class ProductsMenu extends StatelessWidget {
   const ProductsMenu({super.key});
 
   @override
@@ -109,8 +109,89 @@ class ProductsMenu extends StatelessWidget{
   }
 }
 
-class ProfileMenu extends StatelessWidget{
+class ProfileMenu extends StatelessWidget {
   const ProfileMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class NavbarNotifier extends ChangeNotifier {
+  int _index = 0;
+
+  int get index => _index;
+  bool _hideBottomNavBar = false;
+
+  set index(int x) {
+    _index = x;
+    notifyListeners();
+  }
+}
+
+class AnimatedNavBar extends StatefulWidget {
+  final List<MenuItem> menuItems;
+  final NavbarNotifier model;
+  final Function(int) onItemTaped;
+
+  const AnimatedNavBar(
+      {Key? key,
+      required this.model,
+      required this.menuItems,
+      required this.onItemTaped})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => AnimatedNavBarState();
+}
+
+class AnimatedNavBarState extends State<AnimatedNavBar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> animation;
+  bool isHidden = false;
+
+  @override
+  void didUpdateWidget(covariant AnimatedNavBar oldWidget) {
+    if (widget.model._hideBottomNavBar != isHidden) {
+      if (!isHidden) {
+        _showBottomNavBar();
+      } else {
+        _hideBottomNavBar();
+      }
+      isHidden = !isHidden;
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _hideBottomNavBar() {
+    _controller.reverse();
+    return;
+  }
+
+  void _showBottomNavBar() {
+    _controller.forward();
+    return;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this)
+      ..addListener(() {
+        setState(() {});
+      });
+    animation = Tween(begin: 0.0, end: 100.0).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
