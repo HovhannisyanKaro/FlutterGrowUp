010ab9915634bd9dd15a61d18a68ca062fe7bc18
Karo Hovhannisyan <hovhannisyankaro1992@gmail.com>
2023-11-24 12:12:25 +0400

    added NetworkBoundResource

--- lib/learning/api/retrofit/api_service.dart
+++ lib/learning/api/retrofit/api_service.dart
@@ -1,8 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 import 'package:retrofit/http.dart';
-
-import 'demo/casino_games_response.dart';
 
 part 'api_service.g.dart';
 
@@ -24,6 +23,10 @@ class ApiClient {
 
   final Dio _dio = Dio();
   late final ApiService service = ApiService(_dio);
+
+  void test() {
+    var response = service.getGames();
+  }
 }
 
 abstract interface class Repository {
@@ -51,32 +54,22 @@ class RepositoryImpl2 extends Repository {
   }
 }
 
-class Resource<T> extends Equatable {
+class NetworkResource<T> extends Equatable {
   final Status status;
   final Exception? error;
   final T? data;
 
-  const Resource({required this.status, this.data, this.error});
+  const NetworkResource({required this.status, this.data, this.error});
 
   @override
   List<Object?> get props => [status, error, data];
+
+  factory NetworkResource.success(T? data) => NetworkResource(status: Status.success, data: data);
+  factory NetworkResource.error(Exception error, T? data) => NetworkResource(status: Status.error, data: data, error: error);
+  factory NetworkResource.loading() => const NetworkResource(status: Status.loading);
 }
 
 enum Status { success, error, loading }
-
-extension ResourceExtension<T> on Resource<T> {
-  Resource<T> success(T data) {
-    return Resource(status: Status.success, data: data);
-  }
-
-  Resource<T> error(Exception error, T data) {
-    return Resource(status: Status.error, data: data, error: error);
-  }
-
-  Resource<T> loading() {
-    return const Resource(status: Status.loading);
-  }
-}
 
 sealed class ApiWrapper<T> extends Equatable {
   @override
@@ -116,4 +109,74 @@ class UnknownError extends ApiWrapper<void> {
   static final UnknownError _instance = UnknownError._();
 
   factory UnknownError() => _instance;
+}
+
+ApiWrapper<T>? parseResponse<T>() {
+  return null;
+}
+
+@JsonSerializable()
+class ResponseBody {
+  @JsonKey(name: _JsonKey.status)
+  final String status;
+  @JsonKey(name: _JsonKey.response)
+  final Map<String, dynamic> response;
+
+  ResponseBody({required this.status, required this.response});
+}
+
+abstract class _JsonKey {
+  static const String status = 'status';
+  static const String response = 'response';
+}
+
+class NetworkBoundResource<ResultType, RequestType> {
+  Future<ResultType?> fetchFromStorage() async{
+    return null;
+  }
+
+  bool shouldFetch(ResultType? result){
+    return true;
+  }
+
+  Future<RequestType?> fetchFromNetwork() async{
+    return null;
+  }
+
+  Future<void> saveNetworkResult(ResultType? result) async{}
+
+  Exception? isError(RequestType? request) {
+    return null;
+  }
+
+  ResultType? processResponse(ResultType? result, RequestType? request){
+    return null;
+  }
+
+  void processErrorResponse(Exception? exception) {}
+
+  Stream<NetworkResource<ResultType>> run() async* {
+    yield(NetworkResource<ResultType>.loading());
+
+    final storageValue = await fetchFromStorage();
+    final fetch = shouldFetch(storageValue);
+
+    final NetworkResource<ResultType> result;
+
+    if (fetch) {
+      final apiResponse = await fetchFromNetwork();
+      final error = isError(apiResponse);
+      if (error == null) {
+        final processedResponse = processResponse(storageValue, apiResponse);
+        await saveNetworkResult(processedResponse);
+        result = NetworkResource.success(storageValue);
+      } else {
+        processErrorResponse(error);
+        result = NetworkResource.error(error, null);
+      }
+    } else {
+      result = NetworkResource.success(storageValue);
+    }
+    yield(result);
+  }
 }
