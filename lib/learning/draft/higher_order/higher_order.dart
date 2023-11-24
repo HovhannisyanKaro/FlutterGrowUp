5ec48b23f20cd54f0d766565e5bb945e20cd7c7f
karohovhannisyan <hovhannisyankaro1992@gmail.com>
2023-11-25 01:49:19 +0400

    added highOrderFunction for parsing httpRequest and handling error
    in Code there are few errors.

--- lib/learning/draft/higher_order/higher_order.dart
+++ lib/learning/draft/higher_order/higher_order.dart
@@ -4,17 +4,18 @@ void main() {
   parseResponseWithHighOrderArgs(simple);
 }
 
-Future<void> parseResponseWithHighOrderArgs<T>(Future<T> Function() request) async{
-  final result = await request().then((it){
+Future<void> parseResponseWithHighOrderArgs<T>(Future<T> Function() request) async {
+  //todo return rightValue
+  final result = request().then((it) {
     //todo print log
-  }).catchError((obj){
+  }).catchError((obj) {
     // non-200 error goes here.
-    switch(obj.runtimeType){
+    switch (obj.runtimeType) {
       case DioException:
         final res = (obj as DioException).response;
         final statusCode = res?.statusCode;
         final statusMessage = res?.statusMessage;
-
+//todo Got error, print that error.
         break;
       default:
         break;
@@ -25,18 +26,3 @@ Future<void> parseResponseWithHighOrderArgs<T>(Future<T> Function() request) asy
 Future<String> simple() async {
   return '';
 }
-
-// client.getTask('2').then((it) {
-// logger.i(it);
-// }).catchError((obj) {
-// // non-200 error goes here.
-// switch (obj.runtimeType) {
-// case DioError:
-// // Here's the sample to get the failed response error code and message
-// final res = (obj as DioError).response;
-// logger.e('Got error : ${res.statusCode} -> ${res.statusMessage}');
-// break;
-// default:
-// break;
-// }
-// });
