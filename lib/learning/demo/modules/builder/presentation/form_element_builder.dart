bf8939d5954126c373a3716b75c2b1b58571ba1d
Karo Hovhannisyan <hovhannisyankaro1992@gmail.com>
2023-11-28 09:52:02 +0400

    started formElementBuilder

--- lib/learning/demo/modules/builder/presentation/form_element_builder.dart
+++ lib/learning/demo/modules/builder/presentation/form_element_builder.dart
@@ -1,1 +1,20 @@
-abstract interface class FormElementBuilder {}
+import 'package:fluttertest/learning/demo/core/manager/base_event.dart';
+import 'package:fluttertest/learning/demo/core/manager/base_state.dart';
+
+abstract interface class FormElementBuilder<Event extends BuilderEvent> {
+  late final String pageTitle;
+  late final String primaryButtonTitle;
+  late final List<List<String>> items;
+
+  bool validate();
+
+  void onNewBuilderEvent(Event event);
+
+  void performDefinitiveAction();
+
+  void clear();
+}
+
+sealed class BuilderEvent extends BaseEvent {}
+
+sealed class BuilderState extends BaseState {}
